c2439a24d6621b4e62c421508b0181cc9f7f1cf6
davorluc <d.lucic4@hotmail.com>
2022-12-20 16:37:44 +0100

    added working countdown on timer

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,18 @@ const results = document.getElementById('current-game-body');
 const prompt = document.getElementById('pick-your-hand');
 const ranklist = document.getElementById('scoreboard-body');
 const handButtons = document.getElementById('hand-buttons');
+const startPage = document.getElementById('start-page');
+const gamePage = document.getElementById('game');
+const countdownParagraph = document.getElementById('countdown');
 
 // TODO: How to keep track of App state?
 let isLoggedIn = false;
 const hands = null;
 let disableHands = false;
+let onStartPage = false;
+let onGamePage = false;
 let disableLogoutButton = false;
+let countdown = '';
 
 // TODO: Create View functions
 
@@ -29,11 +35,20 @@ function generateDrawText() {
     prompt.innerHTML += template;
 }
 
-function changeHandsAvailability() {
-    hands.forEach((button) => {
-        button.disabled = disableHands;
-    });
-    logoutButton.disabled = disableLogoutButton;
+function disableButtons() {
+    const hands = document.getElementsByClassName('hand');
+    for (let i = 0; i < hands.length; i++) {
+        hands[i].disabled= true;
+    }
+    logoutButton.disabled = true;
+}
+
+function enableButtons() {
+    const hands = document.getElementsByClassName('hand');
+    for (let i = 0; i < hands.length; i++) {
+        hands[i].disabled= false;
+    }
+    logoutButton.disabled = false;
 }
 
 function addToTable(result, userInput, computerInput) {
@@ -45,6 +60,7 @@ function addToTable(result, userInput, computerInput) {
                 </tr>
     `;
     results.innerHTML += template;
+    gameCountdown(3);
 }
 
 function createRanking(rankingEntries) {
@@ -61,6 +77,22 @@ function updateRanking() {
     getRankings(createRanking);
 }
 
+function gameCountdown(seconds) {
+    let waiting = true;
+    disableButtons();
+    countdown = `Next game in ${seconds}`;
+    countdownParagraph.innerHTML = countdown;
+    setTimeout(() => {
+        if (seconds === 1) {
+            countdown = '';
+            countdownParagraph.innerHTML = '';
+            enableButtons();
+        } else {
+            gameCountdown(seconds - 1);
+        }
+    }, 1000);
+}
+
 function getUserHand(handName) {
     return HANDS.find((hand) => hand === handName);
 }
@@ -70,16 +102,14 @@ function login() {
     isLoggedIn = true;
     if (username) {
         document.querySelector('#game').classList.toggle('hidden');
-        document.querySelector('#scoreboard').classList.toggle('hidden');
-        document.querySelector('#login').classList.toggle('hidden');
+        document.querySelector('#start-page').classList.toggle('hidden');
     }
 }
 
 function logout() {
         isLoggedIn = false;
         document.querySelector('#game').classList.toggle('hidden');
-        document.querySelector('#scoreboard').classList.toggle('hidden');
-        document.querySelector('#login').classList.toggle('hidden');
+        document.querySelector('#start-page').classList.toggle('hidden');
 }
 
 // TODO: Register Event Handlers
@@ -101,7 +131,6 @@ logoutButton.addEventListener('click', () => {
     logout();
     prompt.innerHTML = '';
     results.innerHTML = '';
-    handButtons.innerHTML = '';
     updateRanking();
 });
 
